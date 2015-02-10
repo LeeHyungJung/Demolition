eb94c1bf7ba97618d5dd6de63560e6e060be1468
LeeHyungJung <lhj5772@gmail.com>
2015-02-10 20:20:07 +0900

    #master base

--- README.md
+++
@@ -1,2 +0,0 @@
-# DemolitionCode
-ASDF Studio - DemolitionCode
--- Source/Demolition/Demolition.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Demolition.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Demolition, "Demolition" );
